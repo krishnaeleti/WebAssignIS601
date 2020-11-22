fd48f565bff21d46d61990b45bdb06fe35bb867d
Krishna Eleti <krishnaeleti@gmail.com>
2020-11-21 20:06:07 -0500

    task: added templates commit 3

--- app/app.py
+++ app/app.py
@@ -1,138 +1,120 @@
 from typing import List, Dict
 import simplejson as json
-import mysql.connector
 from flask import Flask, request, Response, redirect
 from flask import render_template
+from flaskext.mysql import MySQL
+from pymysql.cursors import DictCursor
 
 app = Flask(__name__)
-user = {'username': 'Krishna Eleti'}
+mysql = MySQL(cursorclass=DictCursor)
 
-class MyDb:
-    def __init__(self):
-        config = {
-            'user': 'root',
-            'password': 'root',
-            'host': 'db',
-            'port': '3306',
-            'database': 'snakesData'
-        }
-        self.connection = mysql.connector.connect(**config)
-
-    def closeDb(self):
-        self.connection.close()
-
-    def get_alldata(self):
-        cursor = self.connection.cursor(dictionary=True)
-        cursor.execute('SELECT * FROM snakes')
-        return cursor.fetchall()
-
-    def get_rating(self, rating_id):
-        cursor = self.connection.cursor(dictionary=True)
-        cursor.execute('SELECT * FROM snakes WHERE id=%s', (rating_id,))
-        result = cursor.fetchall()
-        return result[0]
-
-    def update_rating(self, inputData):
-        cursor = self.connection.cursor(dictionary=True)
-        sql_update_query = """UPDATE snakes t SET t.Game_Number = %s, t.Game_Length = %s """
-        cursor.execute(sql_update_query, inputData)
-        self.connection.commit()
-
-    def insert_rating(self, inputData):
-        cursor = self.connection.cursor(dictionary=True)
-        sql_insert_query = """INSERT INTO snakes (`Game_Number`,Game_Length) VALUES (%s, %s) """
-        cursor.execute(sql_insert_query, inputData)
-        self.connection.commit()
-
-    def delete_rating(self, snakes_id):
-        cursor = self.connection.cursor(dictionary=True)
-        sql_delete_query = """DELETE FROM snakes WHERE id = %s """
-        cursor.execute(sql_delete_query, (snakes_id,))
-        self.connection.commit()
+app.config['MYSQL_DATABASE_HOST'] = 'db'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+app.config['MYSQL_DATABASE_DB'] = 'citiesData'
+mysql.init_app(app)
 
 
-db = MyDb()
-
-
-@app.route('./app./templates./index.html')
+@app.route('/', methods=['GET'])
 def index():
-    snakes = db.get_alldata()
-    return render_template("index.html", title='Home', user=user, snakes=snakes)
-
-@app.route('./app./templates./view.html./<int:snakes_id>', methods=['GET'])
-def record_view(snakes_id):
-    snakes = db.get_rating(snakes_id)
-    return render_template('view.html', title='View Form', user=user, snakes=snakes)
+    user = {'username': 'Cities Project'}
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblCitiesImport')
+    result = cursor.fetchall()
+    return render_template('index.html', title='Home', user=user, cities=result)
 
 
-@app.route('./edit./<int:snakes_id>', methods=['GET'])
-def form_edit_get(snakes_id):
-    snakes = db.get_snakes(snakes_id)
-    return render_template('edit.html', title='Edit Form', user=user, snakes=snakes)
+@app.route('/view/<int:city_id>', methods=['GET'])
+def record_view(city_id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblCitiesImport WHERE id=%s', city_id)
+    result = cursor.fetchall()
+    return render_template('view.html', title='View Form', city=result[0])
 
 
-@app.route('/edit/<int:snakes_id>', methods=['POST'])
-def form_update_post(snakes_id):
-    inputData = (request.form.get('Game_Number'), request.form.get('Game_Length'), snakes_id)
-    db.update_snakes(inputData)
+@app.route('/edit/<int:city_id>', methods=['GET'])
+def form_edit_get(city_id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblCitiesImport WHERE id=%s', city_id)
+    result = cursor.fetchall()
+    return render_template('edit.html', title='Edit Form', city=result[0])
+
+
+@app.route('/edit/<int:city_id>', methods=['POST'])
+def form_update_post(city_id):
+    cursor = mysql.get_db().cursor()
+    inputData = (request.form.get('fldName'), request.form.get('fldLat'), request.form.get('fldLong'),
+                 request.form.get('fldCountry'), request.form.get('fldAbbreviation'),
+                 request.form.get('fldCapitalStatus'), request.form.get('fldPopulation'), city_id)
+    sql_update_query = """UPDATE tblCitiesImport t SET t.fldName = %s, t.fldLat = %s, t.fldLong = %s, t.fldCountry = 
+    %s, t.fldAbbreviation = %s, t.fldCapitalStatus = %s, t.fldPopulation = %s WHERE t.id = %s """
+    cursor.execute(sql_update_query, inputData)
+    mysql.get_db().commit()
     return redirect("/", code=302)
 
-@app.route('/rating/new', methods=['GET'])
+@app.route('/cities/new', methods=['GET'])
 def form_insert_get():
-    return render_template('new.html', title='New Rating Form', user=user)
+    return render_template('new.html', title='New City Form')
 
 
-@app.route('/rating/new', methods=['POST'])
+@app.route('/cities/new', methods=['POST'])
 def form_insert_post():
-    inputData = (request.form.get('Game_Number'), request.form.get('Game_Length'))
-    db.insert_snakes(inputData)
+    cursor = mysql.get_db().cursor()
+    inputData = (request.form.get('fldName'), request.form.get('fldLat'), request.form.get('fldLong'),
+                 request.form.get('fldCountry'), request.form.get('fldAbbreviation'),
+                 request.form.get('fldCapitalStatus'), request.form.get('fldPopulation'))
+    sql_insert_query = """INSERT INTO tblCitiesImport (fldName,fldLat,fldLong,fldCountry,fldAbbreviation,fldCapitalStatus,fldPopulation) VALUES (%s, %s,%s, %s,%s, %s,%s) """
+    cursor.execute(sql_insert_query, inputData)
+    mysql.get_db().commit()
     return redirect("/", code=302)
 
-@app.route('/delete/<int:rating_id>', methods=['POST'])
-def form_delete_post(snakes_id):
-    db.delete_snakes(snakes_id)
+@app.route('/delete/<int:city_id>', methods=['POST'])
+def form_delete_post(city_id):
+    cursor = mysql.get_db().cursor()
+    sql_delete_query = """DELETE FROM tblCitiesImport WHERE id = %s """
+    cursor.execute(sql_delete_query, city_id)
+    mysql.get_db().commit()
     return redirect("/", code=302)
 
 
-# API v1
-
-@app.route('/api/v1/ratings')
-def api_ratings() -> str:
-    js = json.dumps(db.get_alldata())
-    resp = Response(js, status=200, mimetype='application/json')
-    return resp
-
-@app.route('/api/v1/ratings/<int:rating_id>', methods=['GET'])
-def api_retrieve(rating_id) -> str:
-    result = db.get_rating(rating_id)
-    json_result = json.dumps(result)
+@app.route('/api/v1/cities', methods=['GET'])
+def api_browse() -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblCitiesImport')
+    result = cursor.fetchall()
+    json_result = json.dumps(result);
     resp = Response(json_result, status=200, mimetype='application/json')
     return resp
 
-@app.route('/api/v1/ratings/', methods=['POST'])
-def api_add() -> str:
-    inputData = (request.form.get('Year'), request.form.get('Score'), request.form.get('Title'))
-    db.insert_rating(inputData)
-    resp = Response(status=200, mimetype='application/json')
+
+@app.route('/api/v1/cities/<int:city_id>', methods=['GET'])
+def api_retrieve(city_id) -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblCitiesImport WHERE id=%s', city_id)
+    result = cursor.fetchall()
+    json_result = json.dumps(result);
+    resp = Response(json_result, status=200, mimetype='application/json')
     return resp
 
 
-@app.route('/api/v1/ratings/<int:rating_id>', methods=['PUT'])
-def api_edit(rating_id) -> str:
-    inputData = (request.form.get('Year'), request.form.get('Score'), request.form.get('Title'), rating_id)
-    db.update_rating(inputData)
+@app.route('/api/v1/cities/', methods=['POST'])
+def api_add() -> str:
     resp = Response(status=201, mimetype='application/json')
     return resp
 
 
-@app.route('/api/ratings/<int:rating_id>', methods=['DELETE'])
-def api_delete(rating_id) -> str:
-    db.delete_rating(rating_id)
+@app.route('/api/v1/cities/<int:city_id>', methods=['PUT'])
+def api_edit(city_id) -> str:
+    resp = Response(status=201, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/cities/<int:city_id>', methods=['DELETE'])
+def api_delete(city_id) -> str:
     resp = Response(status=210, mimetype='application/json')
     return resp
 
 
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True) # set debug=False on deployment
+    app.run(host='0.0.0.0', debug=True)
